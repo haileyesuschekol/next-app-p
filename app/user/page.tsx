e18cfdd995b7fd6f9df637c2a6b23e2de7f5d70b
Haileyesus Chekol <teamdevs7@gmail.com>
2024-09-24 21:16:33 +0100

    fetching data from the server component

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,5 +1,19 @@
-const userPage = () => {
-  return <div>userPage</div>
+interface User {
+  id: number
+  name: string
+}
+
+const userPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const user: User[] = await res.json()
+  return (
+    <div>
+      <h2>users</h2>
+      {user.map((user) => {
+        return <li key={user.id}>{user.name}</li>
+      })}
+    </div>
+  )
 }
 
 export default userPage
