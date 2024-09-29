cdf473a1ad5090c6ebe126d811c235e40f8063b9
Haileyesus Chekol <teamdevs7@gmail.com>
2024-09-29 08:57:23 +0100

    send put request

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { error } from "console"
+import { stat } from "fs"
 import { NextRequest, NextResponse } from "next/server"
 
 interface UserId {
@@ -16,7 +17,20 @@ export function GET(request: NextRequest, { params: { id } }: UserId) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
+  if (!body.name) {
+    return NextResponse.json({ error: "name is require" }, { status: 400 })
+  } else {
+    return NextResponse.json({ id: 3, name: body.name }, { status: 201 })
+  }
+}
+
+export async function PUT(request: NextRequest, { params: { id } }: UserId) {
+  const body = await request.json()
+
   if (!body.name)
-    return NextResponse.json({ error: "name require" }, { status: 400 })
+    return NextResponse.json({ error: "name is required" }, { status: 400 })
+
+  if (id > 10)
+    return NextResponse.json({ error: "invalid id" }, { status: 404 })
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 })
 }
